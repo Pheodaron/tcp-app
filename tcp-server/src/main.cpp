8fa2ed2b109bbe4b16431a08622e3f49c30f0c40
ENKohanko <grindar10101@gmail.com>
2023-07-25 23:14:37 +0300

    Add handling command line

--- tcp-server/src/main.cpp
+++ tcp-server/src/main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <iostream>
+#include <src/helpers.h>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+
+  int port = checkCommandLineArgs(argc, argv);
+
+  QApplication a(argc, argv);
+  MainWindow w(port);
+  w.show();
+  return a.exec();
 }
