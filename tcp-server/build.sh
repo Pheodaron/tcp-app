0f33b4e3139ae5ca9be0581fd26393225dce2f37
Pheodaron <grindar10101@gmail.com>
2023-07-26 14:45:55 +0300

    Added buildig deb packages

--- tcp-server/build.sh
+++ tcp-server/build.sh
@@ -6,3 +6,13 @@ cd ./build
 cmake ../
 make
 find . ! -name "tcp-server" -delete
+
+cd ..
+mkdir ./tcp-server/
+mkdir ./tcp-server/bin
+cp ./build/tcp-server ./tcp-server/bin
+mkdir ./tcp-server/DEBIAN/
+cp ./debian-manifest/control ./tcp-server/DEBIAN/
+
+dpkg-deb --root-owner-group --build tcp-server
+rm -rf ./tcp-server
