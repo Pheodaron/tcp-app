3419c85f1fa9705c21114dba095bb4bc7695d08a
Pheodaron <grindar10101@gmail.com>
2023-07-26 14:42:44 +0300

    Added building deb package

--- tcp-client/build.sh
+++ tcp-client/build.sh
@@ -6,3 +6,13 @@ cd ./build
 cmake ../
 make
 find . ! -name "tcp-client" -delete
+
+cd ..
+mkdir ./tcp-client/
+mkdir ./tcp-client/bin
+cp ./build/tcp-client ./tcp-client/bin
+mkdir ./tcp-client/DEBIAN/
+cp ./debian-manifest/control ./tcp-client/DEBIAN/
+
+dpkg-deb --root-owner-group --build tcp-client
+rm -rf ./tcp-client
